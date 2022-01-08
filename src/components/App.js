327762a4cad56081e6d2fdc0a5cccd6cc98ba370
unknown <khudyakov_ek@mail.ru>
2022-01-08 03:34:08 +0300

    fix id task

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ const App = () => {
   const addTask = userInput => {
     if(userInput) {
       const newItem = {
-        id: Math.random().toString(36).substr(2,9),
+        id: Date.now().toString(),
         task: userInput,
         complete: false
       }
